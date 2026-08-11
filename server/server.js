918ae0fb99a76a460886b6d2f37b582938132470
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-08-11 21:37:54 +0530

    added database connectivity

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 import app from "./src/app.js";
+import connectDB from "./src/db/db.js";
 
 let port = process.env.PORT || 3000;
 
+
+connectDB();
 app.listen(port,() => {
     console.log('server running on port',port);
 })
