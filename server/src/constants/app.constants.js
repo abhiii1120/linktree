0300531efce6c4854ee0e106b284b58a819c6f42
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-08-12 23:36:07 +0530

    added common cookie component

--- server/src/constants/app.constants.js
+++ server/src/constants/app.constants.js
@@ -1,4 +1,25 @@
 export default {
-    PORT:3000,
-    MONGO_URI:"mongodb://localhost:27017/linktree",
-}
+  PORT: 3000,
+  MONGO_URI: "mongodb://localhost:27017/linktree",
+  NODE_ENV:"development",
+  
+};
+
+export const app_config = {
+  cookie: {
+    accessToken: {
+      httpOnly: false,
+      secure: false,
+      sameSite: "lax",
+      path: "/",
+      maxAge: 15 * 60 * 1000,
+    },
+    refreshToken: {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      path: "/",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    },
+  },
+};
---
+++ server/src/shared/utils/authCookies.js
@@ -0,0 +1,12 @@
+import { app_config } from "../../constants/app.constants"
+
+const config = app_config;
+export const setAuthCookies = (res,refreshToken,accessToken) => {
+    res.cookie("accessToken",accessToken,config.cookie.accessToken);
+    res.cookie("refreshToken",refreshToken,config.cookie.refreshToken);
+}
+
+export const clearAuthCookies = () => {
+    res.clearCookie("accessToken",config.cookie.accessToken);
+    res.clearCookie('refreshToken',config.cookie.refreshToken);
+}
